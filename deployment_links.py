774cedf612cd702123caf0dc59da194db6bfb990
Anant <anantcode@gmail.com>
2021-09-22 15:39:34 +0530

    reading url and credentials from file

--- deployment_links.py
+++ deployment_links.py
@@ -1,14 +1,31 @@
 from selenium import webdriver
 from time import sleep
+import json
 
 class TranslnBot:
 	def __init__(self):
 		self.driver = webdriver.Chrome()
 		self.driver.get("https://google.com")
 		sleep(2)
-		
-		f = open("data\login.json", "r")
-		print(f.read())
+
+		# Opening JSON file
+		f = open('data\login.json',)
+
+		# returns JSON object as
+		# a dictionary
+		data = json.load(f)
+
+		url = data['url']
+		user = data['user']
+		pasw = data['pass']
+
+		print(url)
+		print(user)
+		print(pasw)
+
+
+		# Closing file
+		f.close()
 
 
 TranslnBot()
