b4233e20c4549df80bdfec1bd8c69ffa8d5301ee
Anant <anantcode@gmail.com>
2021-09-22 15:29:46 +0530

    Opens a page

--- deployment_links.py
+++ deployment_links.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+from time import sleep
 
 class TranslnBot:
 	def __init__(self):
 		self.driver = webdriver.Chrome()
-		self.driver.get("https://www.netsuite.com")
+		self.driver.get("https://google.com")
+		sleep(2)
 
 
 TranslnBot()
